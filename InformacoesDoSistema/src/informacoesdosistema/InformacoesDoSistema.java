84265912cb754e093766c6faad223b82214f613a
YureFr-BR <YureFr-BR@users.noreply.github.com>
2020-08-23 18:00:46 -0300

    Initial commit

--- InformacoesDoSistema/src/informacoesdosistema/InformacoesDoSistema.java
+++ InformacoesDoSistema/src/informacoesdosistema/InformacoesDoSistema.java
@@ -1,21 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package informacoesdosistema;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.Date;
+import java.util.Locale;
 /**
  *
  * @author YureFr-BR
  */
 public class InformacoesDoSistema {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+ public static void main(String[] args) {
+      Date relogio = new Date();
+        System.out.print("A data e hora do sistema é ");
+        System.out.println(relogio.toString());
+        
+      Locale idioma = Locale.getDefault();
+        System.out.print("O seu sistema está em: ");
+        System.out.println(idioma.getDisplayLanguage());
+        
+      Toolkit resolucao = Toolkit.getDefaultToolkit();
+        Dimension D = resolucao.getScreenSize();
+        System.out.println("A resolução do seu sistema é de " + D.width + "x" + D.height );
+                  
+      
     }
     
 }
