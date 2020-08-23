514c62cb2d43b05ae42177e8d871c1c5bd4ee304
YureFr-BR <YureFr-BR@users.noreply.github.com>
2020-08-23 17:55:55 -0300

    Initial commit

---
+++ InformacoesDoSistema/src/informacoesdosistema/InformacoesDoSistema.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package informacoesdosistema;
+
+/**
+ *
+ * @author yure5
+ */
+public class InformacoesDoSistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
